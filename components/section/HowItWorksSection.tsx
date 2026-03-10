bae7a3ed72d7c16ab049835cd1945364b2fb429e
Sullivan Amadike <amadikesullivan20@gmail.com>
2026-03-10 13:33:11 +0100

    finished landing page

--- components/section/HowItWorksSection.tsx
+++ components/section/HowItWorksSection.tsx
@@ -1,3 +1,6 @@
+"use client";
+import Image from 'next/image';
+import ProviderImage from '@/public/images/works.png';
 import React from 'react';
 
 const steps = [
@@ -10,17 +13,33 @@ const steps = [
 
 export default function HowItWorksSection() {
     return (
-        <section className="w-full py-16 bg-secondary text-white mb-8">
-            <h2 className="text-3xl font-bold text-center mb-8">How It Works</h2>
-            <div className="flex flex-col md:flex-row justify-center items-center gap-8 max-w-5xl mx-auto">
-                {steps.map((step, idx) => (
-                    <div key={step.title} className="flex flex-col items-center text-center max-w-xs">
-                        <div className="w-12 h-12 mb-3 flex items-center justify-center rounded-full bg-white text-secondary font-bold text-xl">{idx + 1}</div>
-                        <h3 className="font-semibold text-lg mb-1">{step.title}</h3>
-                        <p className="text-sm opacity-80">{step.desc}</p>
+        <>
+            <h2 className="text-3xl font-bold text-center text-primary mb-6 mt-10">How It Works</h2>
+            <section className="w-full bg-transparent text-white flex flex-col md:flex-row items-center justify-between gap-12 mx-auto max-w-5xl mb-12">
+                <div>
+                    <div className="flex flex-col gap-4 max-w-5xl">
+                        {steps.map((step, idx) => (
+                            <div key={step.title} className="flex flex-row items-center text-black gap-3">
+                                <div>
+                                    <h3 className="font-semibold text-lg ">{step.title}</h3>
+                                    <p className="text-sm opacity-80">{step.desc}</p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-        </section>
+                </div>
+                <div className="max-w-[500px] w-full shadow-lg py-4 rounded-lg">
+                    <Image
+                        src={ProviderImage}
+                        alt="Provider Image"
+                        layout="responsive"
+                        priority
+                        width={700}
+                        height={475}
+                        className="object-contain w-full h-auto"
+                    />
+                </div>
+            </section>
+        </>
     );
 }
